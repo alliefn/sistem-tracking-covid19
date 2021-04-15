4dee89f0ff16588fcfa22ffaaab769ed7f827f7d
Allief Nuriman <13519221@std.stei.itb.ac.id>
2021-04-15 03:48:33 +0000

    Menambahkan dokumentasi dan finalisasi versi CLI

--- autentikasi_login.py
+++ autentikasi_login.py
@@ -1,79 +1,93 @@
 import mysql.connector
 
-dN = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        passwd=""
-        )
-mycursor = dN.cursor()
+# Jadi fungsi login ini bakal me-set 3 variabel
+# Username, login, sama status_pengguna
+# Username biar tahu itu siapa yang lagi pake
+# status_pengguna dia siapa
+# Login buat bisa akses segala fitur (Karena udah login)
 
-mycursor.execute("DROP DATABASE IF EXISTS virusTrack")
-mycursor.execute("CREATE DATABASE virusTrack")
+def login():
+    dN = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd=""
+            )
+    mycursor = dN.cursor()
 
-dB = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        passwd="",
-        database="virusTrack"
-        )
+    mycursor.execute("DROP DATABASE IF EXISTS virusTrack")
+    mycursor.execute("CREATE DATABASE virusTrack")
 
-mycursor = dB.cursor()
+    dB = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="",
+            database="virusTrack"
+            )
 
-mycursor.execute("CREATE TABLE User (username VARCHAR(255) PRIMARY KEY, nama VARCHAR(255), surel VARCHAR(255), password VARCHAR(20), nomor_telepon VARCHAR(100), role VARCHAR(10))")
+    mycursor = dB.cursor()
 
-# PROGRAM UTAMA
-pilihan = ""
-login = False
-while (pilihan != "exit" and not login):
-    print("Selamat datang di aplikasi Sistem Tracking Corona!\n")
-    print("Pilih 1 untuk sign in")
-    print("Pilih 2 untuk sign up")
-    print("> ",end='')
+    mycursor.execute("CREATE TABLE User (username VARCHAR(255) PRIMARY KEY, nama VARCHAR(255), surel VARCHAR(255), password VARCHAR(20), nomor_telepon VARCHAR(100), role VARCHAR(10))")
 
-    pilihan = input()
+    # PROGRAM UTAMA
+    pilihan = ""
+    login = False
+    while (pilihan != "exit" and not login):
+        print("Selamat datang di aplikasi Sistem Tracking Corona!\n")
+        print("Pilih 1 untuk sign in")
+        print("Pilih 2 untuk sign up")
+        print("> ",end='')
 
-    if (pilihan == '1'):
-        print("\nMasukkan username Anda: ",end='')
-        uname = input()
-        print("Masukkan password Anda: ",end='')
-        passw = input()
-        sql = "SELECT username, password FROM User WHERE username = '" + uname + "' and " + "password = '" + passw + "'"
-        mycursor.execute(sql)
-        hasil = mycursor.fetchall()
+        pilihan = input()
 
-        # Cek ada hasil atau nggak
-        if (len(hasil) == 0):
-            print("Tidak ada yang memenuhi\n")
+        if (pilihan == '1'):
+            print("\nMasukkan username Anda: ",end='')
+            uname = input()
+            print("Masukkan password Anda: ",end='')
+            passw = input()
+            sql = "SELECT username, password, role FROM User WHERE username = '" + uname + "' and " + "password = '" + passw + "'"
+            mycursor.execute(sql)
+            hasil = mycursor.fetchall()
+
+            # Cek ada hasil atau nggak
+            if (len(hasil) == 0):
+                print("Tidak ada yang memenuhi\n")
+            else:
+                i = 0
+                for line in hasil:
+                    for ch in line:
+                        if (ch != '(' and ch != "'"):
+                            ch = ch # Ubah ch ke ch yang udah di"perbaiki"
+                        i += 1
+                        if (i == 3):
+                            status_pengguna = ch
+                print("Selamat datang,", uname)
+                login = True
+        elif (pilihan == '2'):
+            print("Masukkan username Anda: ",end='')
+            uname = input()
+            print("Masukkan nama Anda: ",end='')
+            name = input()
+            print("Masukkan password Anda: ",end='')
+            passw = input()
+            print("Masukkan email Anda: ",end='')
+            surel = input()
+            print("Masukkan nomor telepon Anda: ",end='')
+            noTel = input()
+            sql = "SELECT username FROM User WHERE username = '" + uname + "'"
+            mycursor.execute(sql)
+            hasil = mycursor.fetchall()
+            if (len(hasil) > 0):
+                print("Username sudah digunakan!\n")
+            else:
+                mycursor.execute("INSERT INTO User VALUES (%s, %s, %s, %s, %s, %s)", (uname, name, passw, surel, noTel, "Pengguna"))
+                dB.commit()
+                print("Selamat datang,", uname)
+                print()
+                login = True
+        elif (pilihan == "exit"):
+            print("Sampai jumpa ya!")
         else:
-            print(len(hasil))
-            for line in hasil:
-                for ch in line:
-                    if (ch != '(' and ch != "'"):
-                        print(ch)
-            login = True
-    elif (pilihan == '2'):
-        print("Masukkan username Anda: ",end='')
-        uname = input()
-        print("Masukkan nama Anda: ",end='')
-        name = input()
-        print("Masukkan password Anda: ",end='')
-        passw = input()
-        print("Masukkan email Anda: ",end='')
-        surel = input()
-        print("Masukkan nomor telepon Anda: ",end='')
-        noTel = input()
-        sql = "SELECT username FROM User WHERE username = '" + uname + "'"
-        mycursor.execute(sql)
-        hasil = mycursor.fetchall()
-        if (len(hasil) > 0):
-            print("Username sudah digunakan!\n")
-        else:
-            mycursor.execute("INSERT INTO User VALUES (%s, %s, %s, %s, %s, %s)", (uname, name, passw, surel, noTel, "Pengguna"))
-            dB.commit()
-            print("Selamat datang,", uname)
-            print()
-            login = True
-    elif (pilihan == "exit"):
-        print("Sampai jumpa ya!")
-    else:
-        print("Perintah salah!\n")
+            print("Perintah salah!\n")
+
+# Keperluan debugging
+# login()
