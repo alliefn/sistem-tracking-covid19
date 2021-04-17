34a950a5a3c69dc85d2517296b0100eb0ff9ad9e
Gayuh Tri Rahutami <13519192@std.stei.itb.ac.id>
2021-04-17 23:22:38 +0700

    added back to home button

--- suhu_gui.py
+++ suhu_gui.py
@@ -1,42 +1,50 @@
 import tkinter as tk
 from datetime import datetime
+from util import clearFrame
 
 def menuSuhu(username, mycursor, dB, frame):
-    # window = tk.Tk()
-    # window.title("Covid-19")
-    # window.geometry("800x800")
-    # window.configure(background='#c8eed9')
+    # clearFrame()
+    window = tk.Tk()
+    window.title("Covid-19")
+    window.geometry("800x800")
+    window.configure(background='#c8eed9')
 
-    # #Frame
-    # frame = tk.Frame(window)
-    # frame.pack(side="top", expand=True, fill="both")
-    # frame.config(background = '#c8eed9')
+    #Frame
+    frame = tk.Frame(window)
+    frame.pack(side="top", expand=True, fill="both")
+    frame.config(background = '#c8eed9')
 
+    #Back to home button
+    homeButton = tk.Button(frame, text = "Kembali ke menu utama", highlightthickness = 0, bd = 0, command=lambda uname = username, cursor = mycursor, mydB = dB : uploadSuhu(uname, cursor, mydB))
+    homeButton.config(font=("Calibri", 14, 'bold'))
+    homeButton.config(background='#c8eed9')
+    homeButton.place(x=30, y=10)
+    
     # Title
     title = tk.Label(frame, text="INPUT SUHU HARIAN")
     title.config(font=("Calibri", 20, 'bold'))
     title.config(background='#c8eed9')
-    title.pack(pady=20)
+    title.pack(pady=40)
 
     # Form
     suhuLabel = tk.Label(frame, text="Suhu tubuh Anda hari ini")
     suhuLabel.config(font=("Calibri", 16, 'bold'))
     suhuLabel.config(background='#c8eed9')
-    suhuLabel.place(x=30, y=70)
+    suhuLabel.place(x=30, y=90)
 
     colon1Label = tk.Label(frame, text=":")
     colon1Label.config(font=("Calibri", 16, 'bold'))
     colon1Label.config(background='#c8eed9')
-    colon1Label.place(x=250, y=70)
+    colon1Label.place(x=250, y=90)
 
     suhuEntry = tk.Entry(frame, width="30")
-    suhuEntry.place(x=270, y=75)
+    suhuEntry.place(x=270, y=95)
 
     #Info
     info = tk.Label(frame, text = "")
     info.config(font=("Calibri", 16, 'bold'))
     info.config(background='#c8eed9')
-    info.place(x=30, y=180)   
+    info.place(x=30, y=200)   
 
     def uploadSuhu(username, mycursor, dB):
         val = float(suhuEntry.get())
@@ -54,4 +62,8 @@ def menuSuhu(username, mycursor, dB, frame):
 
     # Button
     insertSuhuButton = tk.Button(frame, text = "Masukkan suhu", command=lambda uname = username, cursor = mycursor, mydB = dB : uploadSuhu(uname, cursor, mydB))
-    insertSuhuButton.place(x=300, y=120)
+    insertSuhuButton.place(x=300, y=140)
+
+    window.mainloop()
+
+menuSuhu(1, 2, 3, 4)
