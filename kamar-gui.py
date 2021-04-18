b687ed309c3331360be4156b5666f559b7d12227
Karel Renaldi <karelrenaldi8@gmail.com>
2021-04-18 15:15:08 +0700

    finish add-edit rs and kamar

---
+++ kamar-gui.py
@@ -0,0 +1,103 @@
+import tkinter as tk
+import tkinter.messagebox
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="kytnsx88",
+    database="rs_covid"
+)
+cursor = db.cursor()
+
+
+def addKamar():
+    idRs = idRumahSakit.get()
+    harga = hargaKamar.get()
+    jumlah = jumlahKamar.get()
+
+    try:
+        if(len(idRs) == 0 and len(harga) == 0 and len(jumlah) == 0):
+            raise Exception("Masukkan Input!")
+        elif(len(idRs) == 0):
+            raise Exception("Input nama id rumah sakit!")
+        elif(len(harga) == 0):
+            raise Exception("Input alamat harga kamar!")
+        elif(len(jumlah) == 0):
+            raise Exception("Input jumlah kamar!")
+        else:
+            query = "SELECT * from rumahsakit WHERE id = %s"
+            cursor.execute(query, (idRs, ))
+            if(cursor.fetchone() == None):
+                raise Exception("ID Rumah Sakit Tidak Valid")
+            query = "INSERT INTO kamar (rumah_sakit_id, harga, jumlah) VALUES (%s, %s, %s)"
+            cursor.execute(query, (idRs, harga, jumlah))
+            db.commit()
+            print(cursor.rowcount, "record inserted.")
+    except Exception as err:
+        tkinter.messagebox.showerror("Error", str(err))
+    finally:
+        idRumahSakitEntry.delete(0, 'end')
+        hargaKamarEntry.delete(0, 'end')
+        jumlahKamarEntry.delete(0, 'end')
+
+
+window = tk.Tk()
+window.title("Simple Text Editor")
+window.geometry("800x800")
+window.configure(background='#c8eed9')
+
+# Title
+title = tk.Label(text="INPUT DATA KAMAR")
+title.config(font=("Calibri", 20, 'bold'))
+title.config(background='#c8eed9')
+title.pack(pady=20)
+
+# Form
+idRumahSakitLabel = tk.Label(text="ID Rumah Sakit")
+idRumahSakitLabel.config(font=("Calibri", 16, 'bold'))
+idRumahSakitLabel.config(background='#c8eed9')
+idRumahSakitLabel.place(x=30, y=70)
+
+hargaKamarLabel = tk.Label(text="Harga Kamar")
+hargaKamarLabel.config(font=("Calibri", 16, 'bold'))
+hargaKamarLabel.config(background='#c8eed9')
+hargaKamarLabel.place(x=30, y=120)
+
+jumlahKamarLabel = tk.Label(text="Jumlah Kamar")
+jumlahKamarLabel.config(font=("Calibri", 16, 'bold'))
+jumlahKamarLabel.config(background='#c8eed9')
+jumlahKamarLabel.place(x=30, y=170)
+
+colon1Label = tk.Label(text=":")
+colon1Label.config(font=("Calibri", 16, 'bold'))
+colon1Label.config(background='#c8eed9')
+colon1Label.place(x=250, y=70)
+
+colon2Label = tk.Label(text=":")
+colon2Label.config(font=("Calibri", 16, 'bold'))
+colon2Label.config(background='#c8eed9')
+colon2Label.place(x=250, y=120)
+
+colon2Label = tk.Label(text=":")
+colon2Label.config(font=("Calibri", 16, 'bold'))
+colon2Label.config(background='#c8eed9')
+colon2Label.place(x=250, y=170)
+
+idRumahSakit = tk.StringVar()
+hargaKamar = tk.StringVar()
+jumlahKamar = tk.StringVar()
+
+idRumahSakitEntry = tk.Entry(textvariable=idRumahSakit, width="30")
+hargaKamarEntry = tk.Entry(textvariable=hargaKamar, width="30")
+jumlahKamarEntry = tk.Entry(textvariable=jumlahKamar, width="30")
+
+idRumahSakitEntry.place(x=270, y=75)
+hargaKamarEntry.place(x=270, y=125)
+jumlahKamarEntry.place(x=270, y=175)
+
+addKamarButton = tk.Button(
+    window, text="Add New Kamar", command=addKamar)
+addKamarButton.place(x=300, y=220)
+
+window.mainloop()
--- update-rumahsakit-gui.py
+++ update-rumahsakit-gui.py
@@ -11,25 +11,31 @@ db = mysql.connector.connect(
 cursor = db.cursor()
 
 
-def addRumahSakit():
+def updateRumahSakit():
+    idRs = idRumahSakit.get()
     nama = namaRumahSakit.get()
     alamat = alamatRumahSakit.get()
 
     try:
-        if(len(nama) == 0 and len(alamat) == 0):
-            raise Exception("Input nama dan alamat rumah sakit!")
+        if(len(nama) == 0 and len(alamat) == 0 and len(idRs) == 0):
+            raise Exception("Lengkapi input!")
         elif(len(nama) == 0):
             raise Exception("Input nama rumah sakit!")
         elif(len(alamat) == 0):
             raise Exception("Input alamat rumah sakit!")
+        elif(len(idRs) == 0):
+            raise Exception("Input id rumah sakit!")
         else:
-            query = "INSERT INTO RUMAHSAKIT (nama, alamat) VALUES (%s, %s)"
-            cursor.execute(query, (nama, alamat))
+            query = "UPDATE rumahsakit SET nama = %s, alamat = %s WHERE id = %s"
+            cursor.execute(query, (nama, alamat, idRs))
             db.commit()
-            print(cursor.rowcount, "record inserted.")
+            if(cursor.rowcount == 0):
+                raise Exception("Not Valid Input")
+            print(cursor.rowcount, "record(s) affected")
     except Exception as err:
         tkinter.messagebox.showerror("Error", str(err))
     finally:
+        idRumahSakitEntry.delete(0, 'end')
         namaRumahSakitEntry.delete(0, 'end')
         alamatRumahSakitEntry.delete(0, 'end')
 
@@ -40,42 +46,55 @@ window.geometry("800x800")
 window.configure(background='#c8eed9')
 
 # Title
-title = tk.Label(text="INPUT DATA RUMAH SAKIT")
+title = tk.Label(text="UPDATE DATA RUMAH SAKIT")
 title.config(font=("Calibri", 20, 'bold'))
 title.config(background='#c8eed9')
 title.pack(pady=20)
 
 # Form
-namaRumahSakitLabel = tk.Label(text="Nama Rumah Sakit")
+namaRumahSakitLabel = tk.Label(text="ID Rumah Sakit")
 namaRumahSakitLabel.config(font=("Calibri", 16, 'bold'))
 namaRumahSakitLabel.config(background='#c8eed9')
 namaRumahSakitLabel.place(x=30, y=70)
 
-alamatRumahSakitLabel = tk.Label(text="Alamat Rumah Sakit")
+namaRumahSakitLabel = tk.Label(text="Nama Rumah Sakit Baru")
+namaRumahSakitLabel.config(font=("Calibri", 16, 'bold'))
+namaRumahSakitLabel.config(background='#c8eed9')
+namaRumahSakitLabel.place(x=30, y=120)
+
+alamatRumahSakitLabel = tk.Label(text="Alamat Rumah Sakit Baru")
 alamatRumahSakitLabel.config(font=("Calibri", 16, 'bold'))
 alamatRumahSakitLabel.config(background='#c8eed9')
-alamatRumahSakitLabel.place(x=30, y=120)
+alamatRumahSakitLabel.place(x=30, y=170)
 
 colon1Label = tk.Label(text=":")
 colon1Label.config(font=("Calibri", 16, 'bold'))
 colon1Label.config(background='#c8eed9')
-colon1Label.place(x=250, y=70)
+colon1Label.place(x=270, y=70)
 
 colon2Label = tk.Label(text=":")
 colon2Label.config(font=("Calibri", 16, 'bold'))
 colon2Label.config(background='#c8eed9')
-colon2Label.place(x=250, y=120)
+colon2Label.place(x=270, y=120)
 
+colon2Label = tk.Label(text=":")
+colon2Label.config(font=("Calibri", 16, 'bold'))
+colon2Label.config(background='#c8eed9')
+colon2Label.place(x=270, y=170)
+
+idRumahSakit = tk.StringVar()
 namaRumahSakit = tk.StringVar()
 alamatRumahSakit = tk.StringVar()
 
+idRumahSakitEntry = tk.Entry(textvariable=idRumahSakit, width="30")
 namaRumahSakitEntry = tk.Entry(textvariable=namaRumahSakit, width="30")
 alamatRumahSakitEntry = tk.Entry(textvariable=alamatRumahSakit, width="30")
-namaRumahSakitEntry.place(x=270, y=75)
-alamatRumahSakitEntry.place(x=270, y=125)
+idRumahSakitEntry.place(x=290, y=75)
+namaRumahSakitEntry.place(x=290, y=125)
+alamatRumahSakitEntry.place(x=290, y=175)
 
 addRumahSakitButton = tk.Button(
-    window, text="Add New RumahSakit", command=addRumahSakit)
-addRumahSakitButton.place(x=300, y=200)
+    window, text="Update RumahSakit", command=updateRumahSakit)
+addRumahSakitButton.place(x=300, y=220)
 
 window.mainloop()
