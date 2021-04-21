447b63707991df8d2c949f51a2f0518070a1afbb
Karel Renaldi <karelrenaldi8@gmail.com>
2021-04-21 23:43:46 +0700

    finish rs

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ class VirusTrack(tk.Tk):
         self.dB = mysql.connector.connect(
             host="localhost",
             user="root",
-            passwd="",
+            passwd="kytnsx88",
             database="trackingCovid"
         )
 
